c1e61696335c4c8cd4ba64af542ff2ec4c2d1ce9
leny <info@flatland.be>
2014-09-22 13:29:37 +0200

    Solution

--- js/script.js
+++ js/script.js
@@ -2,10 +2,77 @@
  *
  * JS Document - /js/script.js
  *
- * coded by [YOUR NAME]
- * started at [DATE]
+ * coded by leny
+ * started at 22/09/2014
  */
 
 /* jshint boss: true, curly: true, eqeqeq: true, eqnull: true, immed: true, latedef: true, newcap: true, noarg: true, browser: true, jquery: true, noempty: true, sub: true, undef: true, unused: true, white: false */
 
-// start your work here.
+( function( $ ) {
+
+    var $trombinoFigures, $commentTemplate, $comments;
+
+    var _startTrombino = function() {
+        var $current = $trombinoFigures.filter( ":visible" ),
+            _ref,
+            $next = ( ( _ref = $trombinoFigures.eq( $current.index() ) ).size() ) ? _ref : $trombinoFigures.first();
+            $current.fadeOut( "fast", function() {
+                $next.fadeIn( "fast" );
+            } );
+    };
+
+    var _fetchComments = function() {
+        $.ajax( {
+            url: "/comments.json",
+            type: "get",
+            responseType: "json",
+            error: function( oXHR, sStatus, oError ) {
+                console.error( "Shit happens:", oError );
+            },
+            success: function( oResponse ) {
+                var _i = -1, oComment, $comment;
+                for( ; oComment = oResponse[ ++_i ] ; ) {
+                    ( $comment = $commentTemplate.clone() )
+                        .hide()
+                        .find( "h4 strong" )
+                            .text( oComment.author.name )
+                            .end()
+                        .find( "p" )
+                            .text( oComment.content )
+                            .end()
+                        .appendTo( $comments )
+                        .fadeIn( "fast" );
+                }
+            }
+        } );
+    };
+
+    var _moderateComment = function( e ) {
+        e.preventDefault();
+        $( this )
+            .parent()
+                .removeClass( "alert-block" )
+                .removeClass( "alert-info" )
+                .addClass( "alert-error" )
+                .empty()
+                .text( "Ce commentaire a été modéré." );
+    };
+
+    $( function() {
+        // step one
+        $( 'a[rel="external"]' ).attr( "target", "_new" );
+
+        // step two
+        ( $trombinoFigures = $( "#trombino figure" ) ).hide().first().show();
+        setInterval( _startTrombino, 3000 );
+
+        // step three
+        $commentTemplate = ( $comments = $( "#comments ol" ) ).find( "li" ).first().clone();
+        _fetchComments()
+        setInterval( _fetchComments, 5000 );
+
+        // step four
+        $comments.on( "click", ".close", _moderateComment );
+    } );
+
+} )( jQuery );
